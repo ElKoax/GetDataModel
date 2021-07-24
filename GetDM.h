6f6eb96142a5a5d77bbb78ebec5f46e05e8cae20
ElKoax <47944778+ElKoax@users.noreply.github.com>
2021-07-24 13:30:49 +0100

    Update GetDM.h

--- GetDM.h
+++ GetDM.h
@@ -1,49 +1,51 @@
-int GDM;
+// Re-Written 
 
-typedef DWORD(__cdecl* gdm2)();
-gdm2 getdatamodel2 = (gdm2)(ASLR(0xE4A530));
-
-
-typedef DWORD(__thiscall* getdatamodel)(DWORD, DWORD);
-getdatamodel r_getdatamodel = (getdatamodel)(ASLR(0xE4A680));
-
-int GetDatamodel()
+class RobloxDataModel
 {
-	DWORD DMPad[2];
-	r_getdatamodel(getdatamodel2(), (DWORD)DMPad);
-	DWORD DM = DMPad[0];
-	return DM + 0x44;
-	/*
-	GetDataModel, Fixed by Shade and Synapse X source code i guess? xD enjoy this.
-	*/
-}
-
-const char* GetClass(int self)
-{
-	return (const char*)(*(int(**)(void))(*(int*)self + 16))();
-}
-
-int FindFirstClass(int Instance, const char* Name)
-{
-	DWORD StartOfChildren = *(DWORD*)(Instance + 0x2C);
-	DWORD EndOfChildren = *(DWORD*)(StartOfChildren + 4);
-
-	for (int i = *(int*)StartOfChildren; i != EndOfChildren; i += 8)
+public:
+	DWORD GetDataModel()
 	{
-		if (memcmp(GetClass(*(int*)i), Name, strlen(Name)) == 0)
+
+		DWORD Array[2]{};
+		r_getdatamodel(r_getdatamodel_2(), (DWORD)Array);
+		return Array[0] + 0xC;
+	}
+
+	const char* GetClass(int self)
+	{
+		return (const char*)(*(int(**)(void))(*(int*)self + 16))();
+	}
+
+	int FindFirstClass(int Instance, const char* Name)
+	{
+		DWORD StartOfChildren = *(DWORD*)(Instance + 0x2C);
+		DWORD EndOfChildren = *(DWORD*)(StartOfChildren + 4);
+
+		for (int i = *(int*)StartOfChildren; i != EndOfChildren; i += 8)
 		{
-			return *(int*)i;
+			if (memcmp(GetClass(*(int*)i), Name, strlen(Name)) == 0)
+			{
+				return *(int*)i;
+			}
 		}
 	}
-}
+};
 
+RobloxDataModel* DM = new RobloxDataModel;
 
+DWORD DataModel;
+DWORD rL;
+DWORD SC;
 
-void getdatamodelaaa()
+DWORD RBX_LuaState(DWORD ScriptContext) { return  *(DWORD*)(ScriptContext +276) - (ScriptContext + 276);} 
+
+void InitScanning()
 {
-	GDM = GetDatamodel();
-	ScriptContext = FindFirstClass(GDM, "ScriptContext");
-	r_L = *(DWORD*)(ScriptContext + 56 * 0 + 164) - (ScriptContext + 56 * 0 + 164); // if ur going to use this for AXON, change "r_L" to m_rL
-	*(DWORD*)(*(DWORD*)(r_L + 112) + 24) = 6;
-	printf("Passed GetDataModel, Enjoy Exploiting! \n");
+	DataModel = DM->GetDataModel();
+	SC = DM->FindFirstClass(DataModel, "ScriptContext");
+	rL = r_lua_newthread(RBX_LuaState(SC));
+	
 }
+
+
+
